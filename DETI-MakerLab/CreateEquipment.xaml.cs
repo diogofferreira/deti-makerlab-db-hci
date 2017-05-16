8d87b4886303ddbf9b17be19b2e7631572fdc66f
Diogo Ferreira <pdiogoferreira@ua.pt>
2017-05-17 00:47:16 +0100

    Added a way to load image with file dialog

--- DETI-MakerLab/CreateEquipment.xaml.cs
+++ DETI-MakerLab/CreateEquipment.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,7 @@ namespace DETI_MakerLab
     public partial class CreateEquipment : Page
     {
         private SqlConnection cn;
+        private String fileName;
         private int _staffID;
 
         public int StaffID
@@ -99,11 +101,27 @@ namespace DETI_MakerLab
             return cn.State == ConnectionState.Open;
         }
 
-        private void create_equipment_button_Click(object sender, RoutedEventArgs e)
+        private void upload_image_button_Click(object sender, RoutedEventArgs e)
         {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.Filter = "All Image Files | *.*";
+            if (dlg.ShowDialog() == true)
+            {
+                fileName = dlg.FileName;
+                equipment_image.Text = fileName.ToString();
+            }
+        }
+
+        private void create_equipment_button_Click(object sender, RoutedEventArgs e)
+        {   
+            // Copy image to project file and produce its path
+            String RunningPath = AppDomain.CurrentDomain.BaseDirectory;
+            String imagePath = string.Format("{0}images\\", System.IO.Path.GetFullPath(System.IO.Path.Combine(RunningPath, @"..\..\"))) + System.IO.Path.GetFileName(fileName);
+            System.IO.File.Copy(fileName, imagePath, true);
+
+            // TODO : create object and pass it to equipment page
             MessageBox.Show("Equipment has been added !");
             StaffWindow window = (StaffWindow)Window.GetWindow(this);
-            // TODO : create object and pass it to equipment page
             //window.goToEquipmentPage(equipment);
         }
     }
